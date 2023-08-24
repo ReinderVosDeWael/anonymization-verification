7a127ce1f95e4164deb09d7008845e9c79bc325b
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-24 14:55:07 -0400

    Return words rather than bools

--- src/anonymization_verification/document.py
+++ src/anonymization_verification/document.py
@@ -32,20 +32,41 @@ class AnoynimityVerifier:
 
     def __init__(
         self,
-        text: str | Iterable[str],
+        text: Iterable[str],
         entity_recognition_model: str | None = SPACY_MODEL,
+        disallowed_words: Iterable[str] | None = None,
+        allowed_entities: Iterable[str] | None = None,
     ) -> None:
         """Initialize the AnoynimityVerifier object.
 
         Args:
             text: An iterable of strings or a string.
-            ner_model: The name of the spaCy model to use for named entity recognition.
+            entity_recognition_model: The name of the spaCy model to use for named entity recognition.
                 If None, no model is loaded.
+            disallowed_words: An iterable of disallowed words. If None, the default list is used.
+            allowed_entities: An iterable of allowed named entities. If None, the default list is used.
+
         """
-        self.text = tuple(text) if isinstance(text, str) else text
+        self.text = " ".join(text)
         self.entity_recognition_model = (
             spacy.load(entity_recognition_model) if entity_recognition_model else None
         )
+
+        if allowed_entities:
+            self.allowed_entities = allowed_entities
+        else:
+            with open(
+                settings.allowed_entities_file, "r", encoding="utf-8"
+            ) as file_buffer:
+                self.allowed_entities = json.load(file_buffer)
+
+        if disallowed_words:
+            self.disallowed_words = disallowed_words
+        else:
+            with open(
+                settings.disallowed_words_file, "r", encoding="utf-8"
+            ) as file_buffer:
+                self.disallowed_words = json.load(file_buffer)
 
     def contains_any_disallowed(self) -> bool:
         """Check if the document passes all checks.
@@ -53,57 +74,41 @@ class AnoynimityVerifier:
         Returns:
             True if the document passes all checks, False otherwise.
         """
-        return self.contains_disallowed_words() or self.contains_named_entities()
+        return self.find_disallowed_words() or self.find_named_entities()
 
-    def check_fun(self, fun: Callable[[str], bool]) -> bool:
-        """Check if the document returns True for an arbitrary function.
-
-        Args:
-            fun: A function that takes a string and returns a boolean.
-
-        Returns:
-            True if any paragraphs in the document return True for the function, False otherwise.
-        """
-        disallowed = [paragraph.text for paragraph in self.text if fun(paragraph.text)]
-        if disallowed:
-            print("Found the following disallowed paragraphs:")
-            print(disallowed)
-        return bool(disallowed)
-
-    def contains_disallowed_words(self) -> bool:
+    def find_disallowed_words(self) -> set[str]:
         """Check if the document contains disallowed words.
 
         Returns:
             True if the document contains disallowed words, False otherwise.
         """
-        with open(settings.disallowed_words_file, "r", encoding="utf-8") as file_buffer:
-            disallowed_words = json.load(file_buffer)
+        disallowed = {
+            word for word in self.text.split() if word.lower() in self.disallowed_words
+        }
 
-        def any_disallowed_words(text: str) -> bool:
-            text_split = text.lower().split()
-            return any(word.lower() in text_split for word in disallowed_words)
+        if disallowed:
+            logger.info("Disallowed words:\n%s", "\n".join(disallowed))
 
-        return self.check_fun(any_disallowed_words)
+        return disallowed
 
-    def contains_named_entities(self) -> bool:
-        """Check if the document contains named entities.
+    def find_named_entities(self) -> set[str]:
+        """Find name entities in the document.
 
         Returns:
-            True if the document contains named entities, False otherwise.
+            The detected named entities.
         """
         if not self.entity_recognition_model:
             raise ValueError(
                 "No spaCy model loaded. Please provide a spaCy model name to the constructor."
             )
 
-        with open(settings.allowed_entities_file, "r", encoding="utf-8") as file_buffer:
-            allowed_entities = json.load(file_buffer)
+        doc = self.entity_recognition_model(self.text)
+        entities = {entity.text for entity in doc.ents}
+        disallowed = entities - set(self.allowed_entities)
 
-        def any_named_entities(text: str) -> bool:
-            doc = self.entity_recognition_model(text)
-            return any(ent.text not in allowed_entities for ent in doc.ents)
-
-        return self.check_fun(any_named_entities)
+        if disallowed:
+            logger.info("Disallowed named entities:\n%s", "\n".join(disallowed))
+        return disallowed
 
 
 class WordDocument(AnoynimityVerifier):
@@ -137,6 +142,4 @@ class WordDocument(AnoynimityVerifier):
                 paragraphs.append(paragraph)
             else:
                 paragraphs.append(paragraph.text)
-        super().__init__(
-            self.document.paragraphs, entity_recognition_model=entity_recognition_model
-        )
+        super().__init__(paragraphs, entity_recognition_model=entity_recognition_model)
